f67204ef0788275b937417a8470d48f104d8b3f4
booldook <booldook@gmail.com>
2020-10-27 21:27:00 +0900

    app

--- app.js
+++ app.js
@@ -5,6 +5,32 @@ const path = require('path');
 
 /** 전역변수 *************************/
 const memberRouter = require('./routes/member');
+const sqlRouter = require('./routes/sql');
+
+/** mysql *************************/
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user: '',
+	port: ,
+	password: '',
+	database: ''
+});
+
+connection.connect();
+
+connection.query('SELECT * FROM books', (err, result, field) => {
+	console.log(result[0].id);
+	console.log(result[0].title);
+	console.log(result[0].isbn);
+	console.log(result[0].content);
+	console.log(result[0].writer);
+	console.log(result[0].wdate);
+	console.log(result[0].price);
+	console.log(result[0].amount);
+});
+
+connection.end();
 
 
 /** 서버구동 *************************/
@@ -24,3 +50,4 @@ app.use(express.urlencoded({extended: false}));
 app.use('/', express.static(path.join(__dirname, './public')));
 app.use('/storage', express.static(path.join(__dirname, './uploads')));
 app.use('/member', memberRouter);
+app.use('/sql', sqlRouter);
